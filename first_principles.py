9376b59e253854e940aa414462d331282b5fa362
Hashim Al-Obaidi <84986222+githubhash01@users.noreply.github.com>
2024-03-10 02:16:56 +0000

    Add files via upload

--- first_principles.py
+++ first_principles.py
@@ -16,8 +16,6 @@ training_data = [(linearise(image), label) for image, label in zip(images, label
 
 # a global weight matrix W of shape (784, 10)
 #W = np.matrix(np.random.rand(784, 10) * 0.01)
-# intialsing the matrix to zeros
-W = np.zeros((784, 10))
 
 """
 Showing the first image
@@ -36,55 +34,68 @@ Feedforward:
     Image -> Linearise -> Weight Matrix -> Output Layer -> Softmax 
 """
 
-def logsumexp(x):
-    c = x.max()
-    return c + np.log(np.sum(np.exp(x - c)))
+class Model: 
 
-# softmax with logsumexp trick to avoid overflow and underflow
-def softmax_logsumexp(output_layer):
-    return np.exp(output_layer - logsumexp(output_layer))
+    def __init__(self, training_data, alpha, epochs=10): 
+        self.training_data = training_data
+        self.alpha = alpha
+        self.epochs = epochs
+        self.W = np.zeros((784, 10))
 
-# feedforward function
-def feedForward(image): 
-    global W
-    # linearise the image
-    linearised_image = linearise(image) # 784, 1 vector
-    # output layer is w^T * x 
-    output_layer = np.array(np.dot(W.T, linearised_image)).flatten()
-    # softmax function on the output layer to get the probabilities
-    probabilities = softmax_logsumexp(output_layer)
-    return probabilities
+    def logsumexp(self, x):
+        c = x.max()
+        return c + np.log(np.sum(np.exp(x - c)))
 
-# given the ground truth, calculate the log loss
-def logLoss(probabilities, label):
-    return -np.log(probabilities[label] + 1e-10)
+    # softmax with logsumexp trick to avoid overflow and underflow
+    def softmax_logsumexp(self, output_layer):
+        return np.exp(output_layer - self.logsumexp(output_layer))
 
-def avgLogLoss():
-    avg_loss = 0
-    for sample in training_data:
-        x, y = sample
-        avg_loss += logLoss(feedForward(x), y)
-    return avg_loss / len(training_data)
+    # feedforward function
+    def feedForward(self, image): 
+        # linearise the image
+        linearised_image = linearise(image) # 784, 1 vector
+        # output layer is w^T * x 
+        output_layer = np.array(np.dot(self.W.T, linearised_image)).flatten()
+        # softmax function on the output layer to get the probabilities
+        probabilities = self.softmax_logsumexp(output_layer)
+        return probabilities
 
-def update_weights(image, ground_truth, learning_rate):
-    global W
-    # create a new (784, 10) matrix to store the updates
-    updates = np.zeros((784, 10))
-    probabilities = feedForward(image)
-    for digit in range(10):
-        if digit == ground_truth:
-            updates[:, digit] = (probabilities[digit] - 1) * image
-        else:
-            updates[:, digit] = probabilities[digit] * image
-    
-    W = W - learning_rate * updates
+    # given the ground truth, calculate the log loss
+    def logLoss(self, probabilities, label):
+        return -np.log(probabilities[label] + 1e-10)
 
-def epoch(learning_rate):
-    for sample in training_data:
-        image, ground_truth = sample
-        update_weights(image, ground_truth, learning_rate)
+    def avgLogLoss(self):
+        avg_loss = 0
+        for image, label in self.training_data:
+            avg_loss += self.logLoss(self.feedForward(image), label)
+        avg_loss = avg_loss / len(self.training_data)
+        print(avg_loss)
+        return avg_loss
+
+    def update_weights(self, image, ground_truth, learning_rate):
+        # create a new (784, 10) matrix to store the updates
+        updates = np.zeros((784, 10))
+        probabilities = self.feedForward(image)
+        for digit in range(10):
+            if digit == ground_truth:
+                updates[:, digit] = (probabilities[digit] - 1) * image
+            else:
+                updates[:, digit] = probabilities[digit] * image
+        
+        self.W = self.W - learning_rate * updates
+
+    def epoch(self):
+        for sample in self.training_data:
+            image, ground_truth = sample
+            self.update_weights(image, ground_truth, self.alpha)
+
+    def SGD(self):
+        for i in range(self.epochs):
+            self.avgLogLoss()
+            self.epoch()
+        self.avgLogLoss()
 
 
-for i in range(10):
-    print(avgLogLoss())
-    epoch(0.001)
+model = Model(training_data, 0.0000001)
+model.SGD()
+
